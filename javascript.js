ad34880c23f9f29dfc95c08b1c6822523377fd02
Larry <38928644+Larrys-code@users.noreply.github.com>
2023-04-07 07:42:00 +0100

    Create UI for the Rock Paper Scissors game
    Add in DOM manipulation to the javascript code, and alter some functions
    to take in button id's as inputs rather than prompting the user for a
    string input.

--- javascript.js
+++ javascript.js
@@ -26,42 +26,39 @@ function getComputerSelection() {
 }
 
 //playRound {Game of Rock Paper Scissors}
-function playRound() {
-    //create variable playerSelection
-    //ask for user input for playerSelection
-    let playerSelection = prompt("Type Rock, Paper or Scissors! ");
-    //convert playerSelection to lowercase
-    playerSelection = playerSelection.trim().toLowerCase();
-    //make an outcomeMessage
-    let outcomeMessage = "";
+function playRound(playerSelection) {
+
     //make a gameOutcome
     let gameOutcome = "";
-    //if playerSelection isn't legal, gameOutcome is retry. else:
-    if (!(playerSelection==="rock"||playerSelection==="paper"||playerSelection==="scissors")){
-        gameOutcome = "retry";
-    }else{
-        //getComputerSelection as computerSelection
-        let computerSelection = getComputerSelection();
-        //gameOutcome is winOrLose(playerSelection, computerSelection)
-        gameOutcome = winOrLose(playerSelection, computerSelection);
-        //switch gameOutcome === win : `You Win! ${playerSelection} beats ${computerSelection}!`
-        //                       lose : `You Lose! ${computerSelection} beats ${playerSelection}!`
-        //                       draw : `You Drawed!`
-        switch (gameOutcome){
-            case "win" :
-                outcomeMessage = `You Win! ${playerSelection} beats ${computerSelection}!`;
-                break;
-            case "loss" :
-                outcomeMessage = `You Lose! ${computerSelection} beats ${playerSelection}!`;
-                break;
-            case "draw" :
-                outcomeMessage = `You Drawed!`;
-                break;
-        }
-    
-        //alert outcomeMessage
-        alert(outcomeMessage);
+    //getComputerSelection as computerSelection
+    let computerSelection = getComputerSelection();
+
+    switch(playerSelection){
+        case "rock":
+            playerIcon.setAttribute("d", "M19,99L19,83L67,83L67,67L181,67L181,83L197,83L197,149L181,149L181,181L165,181L165,197L149,197L149,213L67,213L67,197L19,197L19,181L3,181L3,99L19,99ZM69,101L53,101L53,179L69,179L69,195L99,195L99,179L115,179L115,165L99,165L99,149L67,149L67,131L99,131L99,115L115,115L115,99L133,99L133,115L149,115L149,131L163,131L163,85L69,85L69,101ZM147,181L117,181L117,195L147,195L147,181ZM179,133L165,133L165,147L179,147L179,133ZM115,133L101,133L101,147L115,147L115,133ZM131,117L117,117L117,131L131,131L131,117ZM163,149L117,149L117,163L163,163L163,149ZM35,101L21,101L21,179L35,179L35,101Z");
+            break;
+        case "paper":
+            playerIcon.setAttribute("d", "M19,99L19,83L67,83L67,67L83,67L83,51L99,51L99,19L115,19L115,3L133,3L133,19L149,19L149,67L229,67L229,83L245,83L245,115L261,115L261,133L245,133L245,165L229,165L229,181L213,181L213,197L197,197L197,213L67,213L67,197L19,197L19,181L3,181L3,99L19,99ZM21,179L35,179L35,101L21,101L21,179ZM53,179L69,179L69,195L195,195L195,181L163,181L163,163L227,163L227,149L163,149L163,131L243,131L243,117L163,117L163,99L227,99L227,85L133,85L133,115L149,115L149,165L131,165L131,117L117,117L117,133L101,133L101,149L67,149L67,131L99,131L99,115L115,115L115,83L131,83L131,21L117,21L117,53L101,53L101,69L85,69L85,85L69,85L69,101L53,101L53,179Z");
+            break;
+        case "scissors":
+            playerIcon.setAttribute("d", "M19,99L19,83L67,83L67,67L229,67L229,83L245,83L245,115L261,115L261,133L245,133L245,149L181,149L181,181L165,181L165,197L149,197L149,213L67,213L67,197L19,197L19,181L3,181L3,99L19,99ZM69,101L53,101L53,179L69,179L69,195L99,195L99,179L115,179L115,165L99,165L99,149L67,149L67,131L99,131L99,115L115,115L115,99L133,99L133,115L149,115L149,131L163,131L163,85L69,85L69,101ZM163,149L117,149L117,163L163,163L163,149ZM147,181L117,181L117,195L147,195L147,181ZM35,101L21,101L21,179L35,179L35,101ZM131,117L117,117L117,131L131,131L131,117ZM115,133L101,133L101,147L115,147L115,133ZM243,117L181,117L181,131L243,131L243,117ZM227,85L181,85L181,99L227,99L227,85Z");
+            break;
     }
+    switch(computerSelection){
+        case "rock":
+            computerIcon.setAttribute("d", "M19,99L19,83L67,83L67,67L181,67L181,83L197,83L197,149L181,149L181,181L165,181L165,197L149,197L149,213L67,213L67,197L19,197L19,181L3,181L3,99L19,99ZM69,101L53,101L53,179L69,179L69,195L99,195L99,179L115,179L115,165L99,165L99,149L67,149L67,131L99,131L99,115L115,115L115,99L133,99L133,115L149,115L149,131L163,131L163,85L69,85L69,101ZM147,181L117,181L117,195L147,195L147,181ZM179,133L165,133L165,147L179,147L179,133ZM115,133L101,133L101,147L115,147L115,133ZM131,117L117,117L117,131L131,131L131,117ZM163,149L117,149L117,163L163,163L163,149ZM35,101L21,101L21,179L35,179L35,101Z");
+            break;
+        case "paper":
+            computerIcon.setAttribute("d", "M19,99L19,83L67,83L67,67L83,67L83,51L99,51L99,19L115,19L115,3L133,3L133,19L149,19L149,67L229,67L229,83L245,83L245,115L261,115L261,133L245,133L245,165L229,165L229,181L213,181L213,197L197,197L197,213L67,213L67,197L19,197L19,181L3,181L3,99L19,99ZM21,179L35,179L35,101L21,101L21,179ZM53,179L69,179L69,195L195,195L195,181L163,181L163,163L227,163L227,149L163,149L163,131L243,131L243,117L163,117L163,99L227,99L227,85L133,85L133,115L149,115L149,165L131,165L131,117L117,117L117,133L101,133L101,149L67,149L67,131L99,131L99,115L115,115L115,83L131,83L131,21L117,21L117,53L101,53L101,69L85,69L85,85L69,85L69,101L53,101L53,179Z");
+            break;
+        case "scissors":
+            computerIcon.setAttribute("d", "M19,99L19,83L67,83L67,67L229,67L229,83L245,83L245,115L261,115L261,133L245,133L245,149L181,149L181,181L165,181L165,197L149,197L149,213L67,213L67,197L19,197L19,181L3,181L3,99L19,99ZM69,101L53,101L53,179L69,179L69,195L99,195L99,179L115,179L115,165L99,165L99,149L67,149L67,131L99,131L99,115L115,115L115,99L133,99L133,115L149,115L149,131L163,131L163,85L69,85L69,101ZM163,149L117,149L117,163L163,163L163,149ZM147,181L117,181L117,195L147,195L147,181ZM35,101L21,101L21,179L35,179L35,101ZM131,117L117,117L117,131L131,131L131,117ZM115,133L101,133L101,147L115,147L115,133ZM243,117L181,117L181,131L243,131L243,117ZM227,85L181,85L181,99L227,99L227,85Z");
+            break;
+    }
+
+    //gameOutcome is winOrLose(playerSelection, computerSelection)
+    gameOutcome = winOrLose(playerSelection, computerSelection);
+
     //return gameOutcome
     return gameOutcome;
 }
@@ -91,43 +88,54 @@ function winOrLose(playerSelection, computerSelection) {
 }
 
 //firstToThree {Rock Paper Scissors match, first to three wins}
-function firstToThree() {
-    //make playerScore and computerScore variables
-    let playerScore = 0;
-    let computerScore = 0;
+function firstToThree(playerSelection) {
     //make a result variable
     let result = "";
-    let scoreMessage = "";
-    //Keep playing whilst playerScore and computerScore are < 3
-    while (playerScore < 3 && computerScore < 3) {
-        //result is the output of playRound
-        result = playRound();
-        switch (result) {
+    //result is the output of playRound
+    result = playRound(playerSelection);
+    switch (result) {
             //if the result of playRound is win +1 to playerScore, report the score
-            case "win" :
-                playerScore = ++playerScore;
-                scoreMessage = `The Score is Player: ${playerScore} Computer: ${computerScore}`;
-                alert(scoreMessage);
-                break;
-            //if the result is loss +1 to computerScore, report the score
-            case "loss" :
-                computerScore = ++computerScore;
-                scoreMessage = `The Score is Player: ${playerScore} Computer: ${computerScore}`;
-                alert(scoreMessage);
-                break;
-            //if the result is draw, report the score
-            default :
-                scoreMessage = `The Score is Player: ${playerScore} Computer: ${computerScore}`;
-                alert(scoreMessage);
-        }
+        case "win" :
+            playerScore = ++playerScore;
+            playerScoreDisplay.textContent = `${playerScore}`;
+            computerScoreDisplay.textContent = `${computerScore}`;
+            resultMessage.textContent = "nice";
+            break;
+        //if the result is loss +1 to computerScore, report the score
+        case "loss" :
+            computerScore = ++computerScore;
+            computerScoreDisplay.textContent = `${computerScore}`;
+            playerScoreDisplay.textContent = `${playerScore}`;
+            resultMessage.textContent = "oof";
+            break;
+        //if the result is draw, report the score
+        default :
+            computerScoreDisplay.textContent = `${computerScore}`;
+            playerScoreDisplay.textContent = `${playerScore}`;
+            resultMessage.textContent = "···";
     }
+    
     //if playerScore === 3, report victory
     if (playerScore === 3){
-        alert("YOU WON!");
+        playerScore = 0;
+        computerScore = 0;
+        resultMessage.textContent = "WINNER!";
         return "win";
     }else if (computerScore === 3){
     //if computerScore === 3, report defeat
-        alert("YOU LOST!");
+        playerScore = 0;
+        computerScore = 0;
+        resultMessage.textContent = "LOSER!";       
         return "loss";
     }
 }
+let playerScore = 0;
+let computerScore = 0;
+const playerIcon = document.getElementById("playerChoice").firstElementChild;
+const computerIcon = document.getElementById("computerChoice").firstElementChild;
+const playerScoreDisplay = document.getElementById("playerScore");
+const computerScoreDisplay = document.getElementById("computerScore");
+const resultMessage = document.querySelector(".message");
+const choiceButton = document.querySelectorAll(".rpsButton");
+choiceButton.forEach(button => button.addEventListener("click", function(){firstToThree(this.id)}));
+console.log(playerIcon.getAttribute("d"));
